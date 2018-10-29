f78f4a47e88a5c541828f235da09c54fd9482563
MasakiMurata <muratamasaki@MasakiMurata.local>
2018-10-29 23:49:26 +0900

    no message

--- nih.py
+++ nih.py
@@ -6,13 +6,15 @@ Created on Mon Oct 29 16:57:22 2018
 """
 
 import numpy as np
-import pandas, csv
+import pandas as pd
+import csv
 from PIL import Image
 
 def set_label():
     path_to_nih_data_csv = "../nih_data/nih_data_000.csv"
     path_to_png_dir = "../nih_data/pngs/"
 #    name_png = "%08d_000.png"
+    num_pngs = len(pd.read_csv(path_to_nih_data_csv))
     nih_csv = open(path_to_nih_data_csv, 'r', encoding="utf-8")
     reader = csv.reader(nih_csv)
     header = next(reader)
