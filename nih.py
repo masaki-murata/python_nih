b2249714cb113102d7fae88f84b4583489113839
DESKTOP-Q4BKBCT\murata <m.murata1982@gmail.com>
2018-10-29 17:21:43 +0900

    no message

--- nih.py
+++ nih.py
@@ -6,5 +6,19 @@ Created on Mon Oct 29 16:57:22 2018
 """
 
 import numpy as np
-import pandas
+import pandas, csv
+from PIL import Image
 
+def set_label():
+    path_to_nih_data_csv = "../nih_data/nih_data_000.csv"
+    path_to_png_dir = "../nih_data/pngs/"
+#    name_png = "%08d_000.png"
+    nih_csv = open(path_to_nih_data_csv, 'r', encoding="utf-8")
+    reader = csv.reader(nih_csv)
+    header = next(reader)
+    print("aho")
+    for row in reader:
+        print(path_to_png_dir+row[0])
+#        Image.open(path_to_png_dir + name_png % id)
+
+set_label()
