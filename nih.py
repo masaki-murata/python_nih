d4c419a92bdb0859757dc9c515355d74a25d7971
DESKTOP-Q4BKBCT\murata <m.murata1982@gmail.com>
2018-10-30 11:17:52 +0900

    no message

--- nih.py
+++ nih.py
@@ -10,6 +10,7 @@ import pandas as pd
 import csv
 from PIL import Image
 
+"""
 def set_label(path_to_nih_data_csv = "../nih_data/nih_data_000.csv",
               path_to_png_dir = "../nih_data/pngs/",
               ):
@@ -20,7 +21,7 @@ def set_label(path_to_nih_data_csv = "../nih_data/nih_data_000.csv",
     nih_csv = open(path_to_nih_data_csv, 'r', encoding="utf-8")
     reader = csv.reader(nih_csv)
     header = next(reader)
-    no_findings = []
+#    no_findings = []
     gts = np.ones(num_pngs, dtype=np.int)
     count = 0
     for row in reader:
@@ -29,13 +30,27 @@ def set_label(path_to_nih_data_csv = "../nih_data/nih_data_000.csv",
 #        print(row[0], img.shape)
         if row[1] == "No Finding":
 #            print(row[1])
-            no_findings.append(row[0])
+#            no_findings.append(row[0])
             gts[count] = 0
         count += 1
     if args[if_save_gts]:
         np.save(args[path_to_gts],gts)
 #    print(len(no_findings))
     return gts
+"""
+
+def set_gts(path_to_nih_data_csv = "../nih_data/Data_Entry_2017_murarta.csv",
+            path_to_png_dir = "../nih_data/pngs/",
+            path_to_gts = "../nih_data/gts.npy",
+            if_save = False,
+            ):
+    df = pd.read_csv(path_to_nih_data_csv)
+    gts = np.array(df['gt'].values, dtype=np.int)
+    if if_save:
+        np.save(path_to_gts, gts)
+    
+    return gts
+        
         
 def test(aho="aho", **args):
     print(args)
@@ -44,4 +59,5 @@ def test(aho="aho", **args):
     else:
         print("no aho")
 #gts = set_label()
-test("aho1", baka="kaba")
+gts = set_gts()
+print(gts.shape)
