f8ce0ac24cf994291a2cfccf977126ce7988ac1e
DESKTOP-Q4BKBCT\murata <m.murata1982@gmail.com>
2018-10-30 13:09:41 +0900

    no message

--- nih.py
+++ nih.py
@@ -7,7 +7,7 @@ Created on Mon Oct 29 16:57:22 2018
 
 import numpy as np
 import pandas as pd
-import csv
+import csv, shutil
 from PIL import Image
 
 """
@@ -39,18 +39,45 @@ def set_label(path_to_nih_data_csv = "../nih_data/nih_data_000.csv",
     return gts
 """
 
+# ground truth を作る関数
 def set_gts(path_to_nih_data_csv = "../nih_data/Data_Entry_2017_murarta.csv",
             path_to_png_dir = "../nih_data/pngs/",
             path_to_gts = "../nih_data/gts.npy",
             if_save = False,
             ):
+    
     df = pd.read_csv(path_to_nih_data_csv)
     gts = np.array(df['gt'].values, dtype=np.int)
     if if_save:
         np.save(path_to_gts, gts)
     
     return gts
+    
+def get_first_image(path_to_nih_data_csv = "../nih_data/Data_Entry_2017_murata.csv",
+                    path_to_png_dir = "../nih_data/pngs/",
+                    path_to_images = "../nih_data/images.npy",
+                    if_save = False,
+                    ):
+    df = pd.read_csv(path_to_nih_data_csv)
+    images = np.zeros((len(df),1024,1024))
+    count = 0
+    for image_index in df['Image Index'].values:
+        images[count]  = np.asarray(Image.open(path_to_png_dir+image_index).convert('L'))
+        count += 1
+    
+    if if_save:
+        np.save(path_to_images, images)
+
+    return images
         
+def move_images(path_to_original_dir="/mnt/nas-public/nih-cxp-dataset/images/",
+                path_to_moved_dir="../nih_data/images/",
+                path_to_nih_data_csv = "../nih_data/Data_Entry_2017_murata.csv",
+                ):
+    df = pd.read_csv(path_to_nih_data_csv)
+    for image_index in df['Image Index'].values:
+        shutil.copyfile(path_to_original_dir+image_index, path_to_moved_dir+image_index)
+    
         
 def test(aho="aho", **args):
     print(args)
@@ -59,5 +86,7 @@ def test(aho="aho", **args):
     else:
         print("no aho")
 #gts = set_label()
-gts = set_gts()
-print(gts.shape)
+move_images()
+#gts = set_gts(if_save=True)
+#images = 
+#print(gts.shape)
