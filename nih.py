dac64638c14da9b3a0bd4dbc84253fdceca9673b
MasakiMurata <muratamasaki@MasakiMurata.local>
2018-10-30 09:57:39 +0900

    add a few lines

--- nih.py
+++ nih.py
@@ -10,7 +10,8 @@ import pandas as pd
 import csv
 from PIL import Image
 
-def set_label():
+def set_label(**args,
+              ):
     path_to_nih_data_csv = "../nih_data/nih_data_000.csv"
     path_to_png_dir = "../nih_data/pngs/"
 #    name_png = "%08d_000.png"
@@ -18,16 +19,28 @@ def set_label():
     nih_csv = open(path_to_nih_data_csv, 'r', encoding="utf-8")
     reader = csv.reader(nih_csv)
     header = next(reader)
-    print("aho")
     no_findings = []
+    gts = np.ones(num_pngs, dtype=np.int)
+    count = 0
     for row in reader:
 #        print(path_to_png_dir+row[0])
         img = np.asarray(Image.open(path_to_png_dir+row[0]).convert('L'))
-        print(row[0], img.shape)
+#        print(row[0], img.shape)
         if row[1] == "No Finding":
-            print(row[1])
+#            print(row[1])
             no_findings.append(row[0])
-    print(len(no_findings))
+            gts[count] = 0
+        count += 1
+    if args[if_save_gts]:
+        np.save(args[path_to_gts],gts)
+#    print(len(no_findings))
+    return gts
         
-
-set_label()
+def test(aho="aho", **args):
+    print(args)
+    if "aho" in args:
+        print("aho is here")
+    else:
+        print("no aho")
+#gts = set_label()
+test("aho1", baka="kaba")
