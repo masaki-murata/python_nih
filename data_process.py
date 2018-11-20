fc56675c1c04da1547ad782592ec7fd5d13e7ec8
DESKTOP-Q4BKBCT\murata <m.murata1982@gmail.com>
2018-11-20 15:28:52 +0900

    no message

--- data_process.py
+++ data_process.py
@@ -8,11 +8,15 @@ Created on Tue Nov 20 14:36:03 2018
 from PIL import Image, ImageOps, ImageDraw
 import pandas as pd
 import numpy as np
+import os
 
 def make_bb_images(path_to_bb = "../nih_data/BBox_List_2017.csv",
                    pathology="Effusion"):
     path_to_images = "../nih_data/images/"
     path_to_bb_images = "../nih_data/bb_images/" + pathology +"/"
+    
+    if os.path.exists(path_to_bb_images):
+        os.makedirs(path_to_bb_images)
     df = pd.read_csv(path_to_bb)
     df_pathology = df[df["Finding Label"]==pathology]    
     pathology_indices = df_pathology["Image Index"].values
@@ -26,7 +30,8 @@ def make_bb_images(path_to_bb = "../nih_data/BBox_List_2017.csv",
             image = Image.open(path_to_images+pathology_index)
             image = ImageOps.colorize(image, black=[0,0,0], white=[255,255,255])
             draw = ImageDraw.Draw(image)
-            [x,y,w,h] = df_index.iloc[:,[2,3,4,5]].values
+#            print(df_index.iloc[:,[2,3,4,5]].values[0])
+            [x,y,w,h] = list(df_index.iloc[:,[2,3,4,5]].values[0])
             draw.rectangle([x, y, x+w, y+h], outline=(255,255,0))
             image.save(path_to_bb_images+pathology_index)
             count+=1
