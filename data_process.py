5dd65ef625dbe989e8f89272e617ca328357bc24
DESKTOP-Q4BKBCT\murata <m.murata1982@gmail.com>
2018-11-20 15:25:23 +0900

    no message

--- data_process.py
+++ data_process.py
@@ -17,7 +17,7 @@ def make_bb_images(path_to_bb = "../nih_data/BBox_List_2017.csv",
     df_pathology = df[df["Finding Label"]==pathology]    
     pathology_indices = df_pathology["Image Index"].values
 
-#    pathology_unique_indices = []
+    count=0
     for pathology_index in pathology_indices:
         df_index = df[df["Image Index"].isin([pathology_index])]
         multiplicity = len(df_index)
@@ -26,6 +26,15 @@ def make_bb_images(path_to_bb = "../nih_data/BBox_List_2017.csv",
             image = Image.open(path_to_images+pathology_index)
             image = ImageOps.colorize(image, black=[0,0,0], white=[255,255,255])
             draw = ImageDraw.Draw(image)
-            x,y,w,h = df_index.iloc[:,[2,3,4,5]].values
+            [x,y,w,h] = df_index.iloc[:,[2,3,4,5]].values
             draw.rectangle([x, y, x+w, y+h], outline=(255,255,0))
             image.save(path_to_bb_images+pathology_index)
+            count+=1
+    return count
+
+pathologies = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Effusion', 'Emphysema', 'Fibrosis', 'Hernia', 'Infiltration', 'Mass', 'Nodule',
+               'Pleural_Thickening', 'Pneumonia', 'Pneumothorax']          
+               
+for pathology in pathologies:
+    count = make_bb_images(pathology=pathology)
+    print(pathology, count)
