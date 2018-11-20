43ad45e8235a8a13e18391e8712977114ff19166
DESKTOP-Q4BKBCT\murata <m.murata1982@gmail.com>
2018-11-20 15:21:38 +0900

    no message

---
+++ data_process.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 20 14:36:03 2018
+
+@author: murata
+"""
+
+from PIL import Image, ImageOps, ImageDraw
+import pandas as pd
+import numpy as np
+
+def make_bb_images(path_to_bb = "../nih_data/BBox_List_2017.csv",
+                   pathology="Effusion"):
+    path_to_images = "../nih_data/images/"
+    path_to_bb_images = "../nih_data/bb_images/" + pathology +"/"
+    df = pd.read_csv(path_to_bb)
+    df_pathology = df[df["Finding Label"]==pathology]    
+    pathology_indices = df_pathology["Image Index"].values
+
+#    pathology_unique_indices = []
+    for pathology_index in pathology_indices:
+        df_index = df[df["Image Index"].isin([pathology_index])]
+        multiplicity = len(df_index)
+        if multiplicity==1:
+#            pathology_unique_indices.append(pathology_index)
+            image = Image.open(path_to_images+pathology_index)
+            image = ImageOps.colorize(image, black=[0,0,0], white=[255,255,255])
+            draw = ImageDraw.Draw(image)
+            x,y,w,h = df_index.iloc[:,[2,3,4,5]].values
+            draw.rectangle([x, y, x+w, y+h], outline=(255,255,0))
+            image.save(path_to_bb_images+pathology_index)
