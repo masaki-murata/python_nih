c8b1b142d95f8aa673885751d9efb82cbe0f0cda
DESKTOP-Q4BKBCT\murata <m.murata1982@gmail.com>
2018-11-20 16:46:04 +0900

    no message

--- interpretable.py
+++ interpretable.py
@@ -17,4 +17,31 @@ def grad_cam(data, label,
     class_idx = np.argmax(predictions[0])
     class_output = model.output[:, class_idx]
 
-    conv_output = model.get_layer(layer_name).output
+    conv_output = model.get_layer(layer_name).output  # layer_nameのレイヤーのアウトプット
+    grads = K.gradients(class_output, conv_output)[0]  # gradients(loss, variables) で、variablesのlossに関しての勾配を返す
+    gradient_function = K.function([model.input], [conv_output, grads])  # model.inputを入力すると、conv_outputとgradsを出力する関数
+    
+    output, grads_val = gradient_function([data])
+    output, grads_val = output[0], grads_val[0]
+
+    # 重みを平均化して、レイヤーのアウトプットに乗じる
+    weights = np.mean(grads_val, axis=(0, 1)) # global average pooling
+    cam = np.dot(output, weights)
+    
+
+    """
+    # 画像化してヒートマップにして合成
+
+    cam = cv2.resize(cam, (200, 200), cv2.INTER_LINEAR) # 画像サイズは200で処理したので
+    cam = np.maximum(cam, 0) 
+    cam = cam / cam.max()
+
+    jetcam = cv2.applyColorMap(np.uint8(255 * cam), cv2.COLORMAP_JET)  # モノクロ画像に疑似的に色をつける
+    jetcam = cv2.cvtColor(jetcam, cv2.COLOR_BGR2RGB)  # 色をRGBに変換
+    jetcam = (np.float32(jetcam) + x / 2)   # もとの画像に合成
+    """
+
+    return jetcam
+
+    
+    
