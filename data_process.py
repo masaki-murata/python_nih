183a0a86124b4901934fa0ffb3d12d53212b116f
DESKTOP-Q4BKBCT\murata <m.murata1982@gmail.com>
2018-12-06 20:35:35 +0900

    no message

--- data_process.py
+++ data_process.py
@@ -40,7 +40,7 @@ def make_bb_images(path_to_bb = "../nih_data/BBox_List_2017.csv",
 def move_cam_pngs(cam_method, layer_name, pathology,
                   path_to_bb_murata = "../nih_data/bb_images/%s/murata_select/", # % pathology
                   path_to_cam_pngs = "../nih_data/models/mm11dd26_size256/%s/cams/%s_%s/TP/", # % (pathology, cam_method, layer_name) 
-#                  path_to_cam_moved = "./nih_data\models\mm11dd26_size256/%s/cams/%s_%s/murata_select/",
+#                  path_to_cam_moved = "../nih_data/models/mm11dd26_size256/%s/cams/%s_%s/murata_select/",
                   ):
     path_to_bb_murata=path_to_bb_murata % pathology
     path_to_cam_pngs = path_to_cam_pngs % (pathology, cam_method, layer_name) 
@@ -62,15 +62,50 @@ def move_cam_pngs(cam_method, layer_name, pathology,
             shutil.copyfile(path_to_cam_pngs+png, path_to_cam_moved+png[:-4]+"_cam.png")
             shutil.copyfile(path_to_bb_murata+png, path_to_cam_moved+png)
     
+def glue_cams(cam_method, layer_name, pathology,
+              path_to_cam_pngs="../nih_data/models/mm11dd26_size256/%s/cams/%s_%s/murata_select/",  # % (pathology, cam_method, layer_name) 
+              path_to_cams = "../nih_data/models/mm11dd26_size256/%s/cams/%s_%s.png",  # % (pathology, cam_method, layer_name) 
+              ):
+    pngs, cam_pngs = [], []
+    for png in os.listdir(path_to_cam_pngs % (pathology, cam_method, layer_name) ):
+        if re.match(".*.png$", png): 
+            if re.match(".*_cam.png$", png): 
+                cam_pngs.append(png)
+            else:
+                pngs.append(png)
+    pngs.sort(), cam_pngs.sort()
+    assert len(pngs) == len(cam_pngs), print( len(pngs), len(cam_pngs) )
+    row_num = int(np.sqrt(len(pngs)/2.0))
+    column_num_half = int( len(pngs) / 2.0 / row_num - 0.01) + 1
+    canvas = Image.new("L", (row_num*128, column_num_half*256))
+    print(row_num, column_num_half)
+    r,c = 0,0
+    for i in range(len(pngs)//2):
+        print(r,c)
+        img = Image.open(path_to_cam_pngs % (pathology, cam_method, layer_name) +pngs[i]).convert('L').resize((128,128))
+        img_cam = Image.open(path_to_cam_pngs % (pathology, cam_method, layer_name) +cam_pngs[i]).convert('L').resize((128,128))
+        canvas.paste(img, (r*128, c*128))
+        canvas.paste(img_cam, (r*128, (c+1)*128))
+        if c==column_num_half-1:
+            r+=1
+            c=0
+        else:
+            c+=1
+    canvas.save(path_to_cams % (pathology, cam_method, layer_name) )
+#        r += 1
+#        c += 1
+#        r = r % row_num
+#        c = c % column_num_half
+
 
 def main():
     layer_names = ["block4_conv4", "block5_conv4", "block5_pool"]
-    cam_methods = ["grad_cam+2"]
+    cam_methods = ["grad_cam", "grad_cam+", "grad_cam+2", "grad_cam_murata"]
     pathology="Effusion"
 #    for layer_name, cam_method in zip(layer_names, cam_methods):
     for layer_name in layer_names:
         for cam_method in cam_methods:
-            move_cam_pngs(cam_method, layer_name, pathology)
+            glue_cams(cam_method, layer_name, pathology)
 #    pathologies = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Effusion', 'Emphysema', 'Fibrosis', 'Hernia', 'Infiltration', 'Mass', 'Nodule',
 #                   'Pleural_Thickening', 'Pneumonia', 'Pneumothorax']          
 #                   
